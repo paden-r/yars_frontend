5794b39f861dcea1548ed283f14f15e1db77a383
Paden Rose <paden.rose@q2.com>
2022-11-14 16:28:49 -0600

    Initial push

---
+++ src/components/Chip.js
@@ -0,0 +1,3 @@
+import React from 'react';
+const Chip = ({ label }) => <p className='chip'>{label}</p>;
+export default Chip;
---
+++ src/config/Api.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import axios from "axios";
+
+export const postList = async () => {
+    // const url = `
+    //     https://api.buttercms.com/v2/posts?auth_token=${read_token}`;
+    // return axios.get(url).then((res) => {
+    //     return res.data.data;
+    // });
+};
